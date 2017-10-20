a680d257e06e5ef91870e44f7e3eead7015bd053
Marisol Tapasco <marisoltapasco@utp.edu.co>
2017-10-19 20:24:53 -0500

    PSR

---
+++ Procesador2/Psr.vhd
@@ -0,0 +1,58 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    23:50:57 10/19/2017 
+-- Design Name: 
+-- Module Name:    Psr - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Psr is
+    Port ( clk : in  STD_LOGIC;
+           rst : in  STD_LOGIC;
+           nzvc : in  STD_LOGIC_VECTOR (3 downto 0);
+           ncwp : in  STD_LOGIC;
+           cwp : out  STD_LOGIC;
+           c : out  STD_LOGIC);
+end Psr;
+
+architecture Behavioral of Psr is
+
+begin
+process(nzvc,clk,rst, ncwp)
+	begin
+		if(rst = '1')then
+			cwp <= '0';
+			c <= '0';
+		else
+			if(rising_edge(clk))then
+				cwp <= ncwp;
+				c <= nzvc(0);
+			end if;
+		end if;
+end process;
+
+end Behavioral;
+
