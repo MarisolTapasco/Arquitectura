1febdeb1668c593b7e06feba13be10854209ebb1
JuanDavidLopez <juandavidlc12@gmail.com>
2017-10-26 23:43:20 -0500

    Update ALU.vhd

--- Procesador3/ALU.vhd
+++ Procesador3/ALU.vhd
@@ -33,75 +33,82 @@ use ieee.numeric_std.ALL;
 --use UNISIM.VComponents.all;
 
 entity ALU is
-    Port ( Aluop: in std_logic_vector (5 downto 0);
-	        op1 : in  std_logic_vector (31 downto 0);
-           op2 : in  std_logic_vector (31 downto 0);
-			  c: in std_logic;
-           result : out  std_logic_vector (31 downto 0));
+    Port ( op1 : in  STD_LOGIC_VECTOR (31 downto 0);
+           op2 : in  STD_LOGIC_VECTOR (31 downto 0);
+			  c: in  STD_LOGIC;
+           result : out  STD_LOGIC_VECTOR (31 downto 0);
+           Aluop : in  STD_LOGIC_VECTOR (5 downto 0));
 end ALU;
 
 architecture Behavioral of ALU is
 
 begin
-process (Aluop,op1,op2, c)
-
-begin
-	case Aluop is
-		when "000000" =>
-			result <= std_logic_vector(unsigned(op1) + unsigned(op2))  ;  --ADD
-		when "000001" =>
-			result <= op1 AND op2;		                                   --AND
-		when "000010" =>
-			result <= op1 OR op2;				                             --OR
-		when "000011" =>
-			result <= op1 XOR op2;				                             --XOR
-		when "000100" =>
-			result <= std_logic_vector(unsigned(op1) - unsigned(op2)) ;	  --SUB
-		when "000101" =>
-			result <= op1 AND NOT op2;				                          --ANDN
-		when "000110" =>
-			result <= op1 OR NOT op2;		                                --ORN
-		when "000111" =>
-			result <= op1 XNOR op2 ;				                          --XNOR	
-		when "100101" => 
-			result <= to_stdlogicvector(to_bitvector(op1) SLL conv_integer(op2));             --SLL
-		when "100110" =>
-			result <= to_stdlogicvector(to_bitvector(op1) SRL conv_integer(op2));             --SLR
-		when "010000" => 
-			result <= std_logic_vector((op1) + (op2)); 						  --ADDcc
-		when "010100" => 
-			result <= std_logic_vector((op1) - (op2)); 						  --SUBcc
-		when "010001" => 
-			result <= std_logic_vector((op1) and (op2)); 					  --ANDcc
-		when "010101" => 
-			result <= std_logic_vector((op1)and (not(op2))); 				  --ANDNcc
-		when "010010" => 
-			result <= std_logic_vector((op1) or (op2)); 						  --ORcc
-		when "010110" => 
-			result <= std_logic_vector((op1) or (not(op2))); 			     --ORNcc
-		when "010011" => 
-			result <= std_logic_vector((op1) xor (op2)); 					  --XORcc
-		when "010111" => 
-			result <= std_logic_vector((op1) xnor (op2)); 				     --XNORcc
-		when "001000" => 
-			result <= std_logic_vector((op1) + (op2)+c); 					  --ADDX
-		when "001100" => 
-			result <= std_logic_vector((op1) - (op2)-c); 					  --SUBx
-		when "011000" => 
-			result <= std_logic_vector((op1) + (op2)+c); 					  --ADDxcc
-		when "011100" => 
-			result <= std_logic_vector((op1) - (op2)-c); 					  --SUBxcc
-
-		when "111100" =>                                                 --save
-		   result <= op1 + op2;
+	process(op1,op2,Aluop)
+	begin
+	case (Aluop) is
+                  when "000000" => -- add 
+								result <= op1 + op2;
+                  when "000001" =>--AND
+								result <= op1 and op2;
+						when "000010" =>--OR
+								result <= op1 or op2;
+						when "000011" =>--xor
+								result <= op1 xor op2;
+						when "000111" =>--xnor
+								result <= op1 xnor op2;
+						when  "000100"=>--Sub
+								result <= op1 - op2;
+						when "000101"=>--and not
+								result<=op1 and not op2;
+						when "000110"=>--nor
+								result<= op1 or not op2;
+								
+						when "010000"=>--addcc
+								result <= op1 + op2;
+						when "010100"=>--subcc
+								result <= op1 - op2;
+						when "010001" =>--andcc
+								result<= op1 and op2;
+						when "010101" =>--andncc
+								result<= op1 and not op2;
+						when "010010" => --orcc
+								result <= op1 or op2;
+						when "010110" =>--orncc
+								result <= op1 or not op2;
+						when "001000"=>--addx
+								result <= op1 + op2 + c;
+						when "011000" =>--addxcc
+								result <= op1 + op2 + c;
+						when "001100" => --subx
+								result <= op1- op2 - c;
+						when "011100" => --subxcc
+								result <= op1 - op2 - c;
+								
+						when "010011" => --xorcc
+								result <=  op1 xor op2;
+							when "010111" => --xnorcc
+								result <=  op1 xor op2;		
+						when "100101" => --sll
+								result <= to_stdlogicvector(to_bitvector(op1) sll conv_integer(op2));
 						
-		when "111101" =>                                                 --restore
-         result <= op1 + op2;	
-	 when others =>
-	  result <="00000000000000000000000000000000";
-
-	 
-	end case;
-end process;
+						when "100110" => --srl
+								result <= to_stdlogicvector(to_bitvector(op1) srl conv_integer(op2));		
+						
+						when "111100" => --save
+								result <= op1 + op2;
+						
+						when "111101" => --restore
+								result <= op1 + op2;
+						when "011011" => -- jmpl
+							result <=op1 + op2;
+							
+						when "111110" => --st y --ld
+							result <= op1 + op2;
+						when others => --nops
+				          result<= x"00000000";
+	
+				end case;	
+	  end process; 
 end Behavioral;
+
 
